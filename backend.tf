92ecf69694f0771d31e34e02ead824e293574bbc
Sai Krishna <152583402+saikrishna844@users.noreply.github.com>
2026-08-01 03:38:02 +0530

    Add Terraform CD planning workflow

--- backend.tf
+++ backend.tf
@@ -6,6 +6,7 @@ terraform {
     key                  = "azure-enterprise-landing-zone.tfstate"
 
     subscription_id = "31e04be6-4545-4ac3-88c4-cc79f284f7ac"
-    tenant_id       = "d77ec4e8-c88b-431b-8d7f-77755a4bbefc"
+    tenant_id       = "d77ec4e8-c88b-431b-8d7f-77755a4bbefc" 
+    #use_azuread_auth = true
   }
 }
